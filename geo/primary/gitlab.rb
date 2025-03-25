c6d54d4bd48d7010c3f18cb55cdb91176533ce31
Amaury Yacksmith <13516584-yaker@users.noreply.gitlab.com>
2025-03-25 11:17:33 -0400

    Fix config path for license file
    Remove license file path in gitlab.rb because using default location

--- geo/primary/gitlab.rb
+++ geo/primary/gitlab.rb
@@ -4,4 +4,3 @@ external_url 'http://geo-primary.local'
 # Enable Geo primary role
 gitlab_rails['geo_node_name'] = 'geo-primary'
 gitlab_rails['geo_primary_role'] = true
-gitlab_rails['initial_license_file'] = './Gitlab.gitlab-license'
