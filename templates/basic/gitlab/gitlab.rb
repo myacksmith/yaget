1d8cfb02d8a044975f95aba6295f0ce3ce007c43
Amaury Yacksmith <13516584-yaker@users.noreply.gitlab.com>
2025-05-27 14:32:53 -0400

    Use double quotes instead of single quotes in gitlab.rb

--- templates/basic/gitlab/gitlab.rb
+++ templates/basic/gitlab/gitlab.rb
@@ -1,18 +1,18 @@
 # Basic GitLab configuration
-external_url 'http://gitlab.local'
+external_url "${EXTERNAL_URL}"
 
 # Resource optimization for testing
-puma['worker_processes'] = 2
-puma['min_threads'] = 1
-puma['max_threads'] = 4
+puma["worker_processes"] = 2
+puma["min_threads"] = 1
+puma["max_threads"] = 4
 
-postgresql['shared_buffers'] = '256MB'
-postgresql['max_worker_processes'] = 4
+postgresql["shared_buffers"] = "256MB"
+postgresql["max_worker_processes"] = 4
 
 # Disable unnecessary services for testing
-gitlab_pages['enable'] = false
-registry['enable'] = false
-gitlab_kas['enable'] = false
+gitlab_pages["enable"] = false
+registry["enable"] = false
+gitlab_kas["enable"] = false
 
 # Set time zone
-gitlab_rails['time_zone'] = 'UTC'
+gitlab_rails["time_zone"] = "UTC"
